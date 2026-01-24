282920c402a844e307e16e8357b81dc395083f18
KN99HN <khanhnguyen99hn@gmail.com>
2026-01-24 16:33:29 +0000

    Implement Insert and Find for B-tree
    API has no test cases yet

--- db/b_tree.go
+++ db/b_tree.go
@@ -1,9 +1,16 @@
 package db
 
-type Node interface {
-	Insert(k string, v string) bool
-	Find(k string) uint64
-	Delete(k string) bool
+import (
+	"fmt"
+	"slices"
+	"strings"
+)
+
+type TreeNode interface {
+	Insert(k string, v uint64) bool
+	Find(k string) (uint64, error)
+	Balance(k string, low TreeNode, high TreeNode) bool
+	//Delete(k string) bool
 }
 
 type NotFoundError struct {
@@ -11,62 +18,137 @@ type NotFoundError struct {
 }
 
 func (e *NotFoundError) Error() string {
-	return fmt.Sprintf("%s", e.arg, e.message)
+	return fmt.Sprintf("%s", e.message)
 }
 
+type Child struct {
+	key string
+	value uint64
+	left TreeNode
+	right TreeNode
+}
+
+type RootNode struct {
+	m int
+	child TreeNode
+}
 
 type InternalNode struct {
 	m int
-	keys [m]string
-	children [m]*Node
+	children []*Child
+	parent TreeNode
 }
 
 type LeafNode struct {
 	m int
-	keys [m]string
-	values [m]uint64
+	children []*Child
+	parent TreeNode
 }
 
-func newInternalNode(m int) *InternalNode {
+func newRootNode(m int) *RootNode {
+	return &RootNode {m, nil}
+}
+
+func newInternalNode(m int, children []*Child, parent TreeNode) TreeNode {
 	return &InternalNode {
-		m, make([]string, m), make([]*Node, m),
+		m, children, parent,
 	}
 }
 
-func binarySearchKeys(k string, keys []string, low int, high int) int {
-	if (high - low) == 1 {
-		if current_key <= k {
-			return low
-		} else {
-			return high
+func (n *RootNode) Balance(k string, low TreeNode, high TreeNode) bool {
+	n.child = newInternalNode(n.m, make([]*Child, 0), nil)
+	return n.child.Balance(k, low, high)
+}
+
+func (n *RootNode) Find(k string) (uint64, error) {
+	return n.child.Find(k)
+}
+
+func (n *RootNode) Insert(k string, v uint64) bool {
+	if n.child != nil {
+		return n.child.Insert(k, v)
+	}
+	children := make([]*Child, 0)
+	n.child = newLeafNode(n.m, children, nil)
+	return n.child.Insert(k, v)
+}
+
+func (n *InternalNode) Balance(k string, low TreeNode, high TreeNode) bool {
+	children := append(n.children, &Child{ k, 0, low, high})
+	slices.SortFunc(children, func(a,b *Child) int {
+		return strings.Compare(a.key, b.key)
+	})
+	if len(children) >= n.m {
+		pivot_index := len(children) / 2
+		low_children := children[0: pivot_index]
+		high_children := children[pivot_index:]
+		low_node := newInternalNode(n.m, low_children, n.parent)
+		high_node := newInternalNode(n.m, high_children, n.parent)
+		if n.parent == nil {
+			n.parent = newRootNode(n.m)
 		}
+		return n.parent.Balance(children[pivot_index].key, low_node, high_node)
 	}
-	half := (high + low) / 2
-	current_key := keys[half]
-	if current_key == k {
-		return half
+	return true
+}
+
+func (n *InternalNode) Insert(k string, v uint64) bool {
+	i, _ := slices.BinarySearchFunc(n.children, &Child {k, 0, nil, nil}, func(a, b *Child) int {
+		return strings.Compare(a.key, b.key)
+	})
+	pivot_key := n.children[i].key
+	if strings.Compare(k, pivot_key) <= 0 {
+		return n.children[i].left.Insert(k, v)
 	}
-	if current_key < k {
-		return binarySearchKeys(k, keys, half, high)
+	return n.children[i].right.Insert(k, v)
+}
+
+func (n *LeafNode) Balance(k string, low TreeNode, high TreeNode) bool {
+	return true
+}
+
+func (n *LeafNode) Insert(k string, v uint64) bool {
+	children := append(n.children, &Child {k, v, nil, nil })
+	slices.SortFunc(children, func(a,b *Child) int {
+		return strings.Compare(a.key, b.key)
+	})
+	if len(children) >= n.m {
+		pivot_index := len(children) / 2
+		low_records := children[0: pivot_index]
+		high_records := children[pivot_index:]
+		low_node := newLeafNode(n.m, low_records, n.parent)
+		high_node := newLeafNode(n.m, high_records, n.parent)
+		if n.parent == nil {
+			n.parent = newRootNode(n.m)
+		}
+		return n.parent.Balance(children[pivot_index].key, low_node, high_node)
 	}
-	return binarySearchKeys(k, keys, low, half)
+	return true
 }
 
 func (n *InternalNode) Find(k string) (uint64, error) {
-	next_child_index := binarySearchKeys(k, n.keys, 0, n.m)
-	return n.children[next_child_index].Find(k)
+	i, _ := slices.BinarySearchFunc(n.children, &Child {k, 0, nil, nil }, func(a, b *Child) int {
+		return strings.Compare(a.key, b.key)
+	})
+	pivot_key := n.children[i].key
+	if strings.Compare(k, pivot_key) <= 0 {
+		return n.children[i].left.Find(k)
+	}
+	return n.children[i].right.Find(k)
 }
 
 func (n *LeafNode) Find(k string) (uint64, error) {
-	current_child_index := binarySearchKeys(k, n.keys, 0, n.m)
-	if current_child_index == -1 {
-		return 0, &NotFoundError{ fmt.Sprintf("No index for %s", k) }
+	i, found := slices.BinarySearchFunc(n.children, &Child{k, 0, nil, nil}, func(a, b *Child) int {
+		return strings.Compare(a.key, b.key)
+	})
+	if !found {
+		return 0, &NotFoundError{ fmt.Sprintf("No record found for %s",k) }
 	}
-	return n.values[current_child_index]
+	return n.children[i].value, nil
 }
 
-func newLeafNode(m int) *LeafNode {
+func newLeafNode(m int, children []*Child, parent TreeNode) TreeNode {
 	return &LeafNode {
-		m, make([]string, m), make([]uint64, m),
+		m, children, parent,
 	}
 }
