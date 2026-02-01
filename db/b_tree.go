5108fad349df2b186f76d94e0ca8ccf05d40f415
KN99HN <khanhnguyen99hn@gmail.com>
2026-02-01 19:14:56 +0000

    Implement quick sort for sorting keys in a node

--- db/b_tree.go
+++ db/b_tree.go
@@ -2,14 +2,16 @@ package db
 
 import (
 	"fmt"
-	"slices"
+	//"slices"
 	"strings"
 )
 
 type TreeNode interface {
-	Insert(k string, v uint64) bool
-	Find(k string) (uint64, error)
-	Balance(k string, low TreeNode, high TreeNode) bool
+	//Insert(k string, v uint64) bool
+	//Find(k string) (uint64, error)
+	//Balance(k string, low TreeNode, high TreeNode) bool
+	SetParent(p TreeNode)
+	String() string
 	//Delete(k string) bool
 }
 
@@ -29,8 +31,27 @@ type Child struct {
 }
 
 func (c Child) String() string {
-	return fmt.Sprintf("{ %s, %d }", c.key, c.value)
+	return fmt.Sprintf("{ %s, %d, %s, %s }", c.key, c.value, c.left, c.right)
 }
+
+func (t InternalNode) String() string {
+	child_string := "{"
+	for _, c := range(t.children) {
+		child_string += fmt.Sprintf("%s,", c)
+	}
+	child_string += "}"
+	return fmt.Sprintf("{ %d, %s }", t.m, child_string)
+}
+
+func (l LeafNode) String() string {
+	child_string := "{"
+	for i, v := range(l.keys) {
+		child_string += fmt.Sprintf("(%s,%s) ", v, l.values[i])
+	}
+	child_string += "}"
+	return fmt.Sprintf("{ %d, %s }", l.m, child_string)
+}
+
 
 type RootNode struct {
 	m int
@@ -39,17 +60,19 @@ type RootNode struct {
 
 type InternalNode struct {
 	m int
-	children []*Child
+	children []TreeNode
+	keys []string
 	parent TreeNode
 }
 
 type LeafNode struct {
 	m int
-	children []*Child
 	parent TreeNode
+	keys []string
+	values []string
 }
 
-func newChild(k string, v uint64, left *TreeNode, right *TreeNode) *Child {
+func newChild(k string, v uint64, left TreeNode, right TreeNode) *Child {
 	return &Child {
 		k, v, left, right,
 	}
@@ -59,14 +82,85 @@ func newRootNode(m int) *RootNode {
 	return &RootNode {m, nil}
 }
 
-func newInternalNode(m int, children []*Child, parent TreeNode) TreeNode {
+func newInternalNode(m int) TreeNode {
 	return &InternalNode {
-		m, children, parent,
+		m, make([]TreeNode, m), make([]string, m - 1), nil,
 	}
 }
 
+func newLeafNode(m int) TreeNode {
+	return &LeafNode {
+		m, nil, make([]string, m - 1), make([]string, m - 1),
+	}
+}
+
+func (n *RootNode) SetParent(p TreeNode) {}
+
+func (n *LeafNode) SetParent(p TreeNode) {
+	n.parent = p
+}
+
+func (n *InternalNode) SetParent(p TreeNode) {
+	n.parent = p
+}
+
+func BinarySearch(keys []string, k string, low int, high int) int {
+	mid := (high + low) / 2
+	if mid < 0 || mid > len(keys) {
+		mid = len(keys) - 1
+	}
+	pivot := keys[mid]
+	res := strings.Compare(pivot, k)
+	if (high - low) == 1 {
+		if res <= 0 {
+			return mid + 1
+		}
+		return mid
+	}
+	if res <= 0 {
+		return BinarySearch(keys, k, mid, high)
+	}
+	return BinarySearch(keys, k, low, mid)
+}
+
+func QuickSort(keys[] string) {
+	QuickSortHelper(keys, 0, len(keys) - 1)
+}
+
+func QuickSortHelper(keys[] string, low int, high int) {
+	if (high - low) < 1 { return }
+	partition_index := (low + high) / 2
+	i := low
+	j := high
+	for i < j {
+		pivot := keys[partition_index]
+		low_val := keys[i]
+		high_val := keys[j]
+		ltp := strings.Compare(low_val, pivot) >= 0
+		gtp := strings.Compare(high_val, pivot) <= 0
+		if ltp && gtp {
+			tmp := keys[j]
+			keys[j] = keys[i]
+			keys[i] = tmp
+			if i == partition_index {
+				partition_index = j
+			}
+			if j == partition_index {
+				partition_index = i
+			}
+			i++
+			j--
+		}
+		if !ltp { i++ }
+		if !gtp { j--}
+	}
+	QuickSortHelper(keys, low, partition_index)
+	QuickSortHelper(keys, partition_index + 1, high)
+}
+
+/*
 func (n *RootNode) Balance(k string, low TreeNode, high TreeNode) bool {
-	n.child = newInternalNode(n.m, make([]*Child, 0), nil)
+	n.child = newInternalNode(n.m, make([]*Child, 0), n)
 	return n.child.Balance(k, low, high)
 }
 
@@ -79,7 +173,7 @@ func (n *RootNode) Insert(k string, v uint64) bool {
 		return n.child.Insert(k, v)
 	}
 	children := make([]*Child, 0)
-	n.child = newLeafNode(n.m, children, nil)
+	n.child = newLeafNode(n.m, children, n)
 	return n.child.Insert(k, v)
 }
 
@@ -88,6 +182,18 @@ func (n *InternalNode) Balance(k string, low TreeNode, high TreeNode) bool {
 	slices.SortFunc(children, func(a,b *Child) int {
 		return strings.Compare(a.key, b.key)
 	})
+	i, _ := slices.BinarySearchFunc(children, &Child{k, 0, nil, nil}, 
+		func(a,b *Child) int { return strings.Compare(a.key, b.key) })
+	if i > 0 && i < (len(children) - 1) {
+		children[i - 1].right = children[i].left
+		children[i + 1].left = children[i].right
+	}
+	if i == 0  && len(children) > 1 {
+		children[i + 1].left = children[i].right
+	}
+	if i == len(children) - 1  && len(children) > 1 {
+		children[i - 1].right = children[i].left
+	}
 	if len(children) > n.m {
 		pivot_index := len(children) / 2
 		low_children := children[0: pivot_index]
@@ -99,14 +205,19 @@ func (n *InternalNode) Balance(k string, low TreeNode, high TreeNode) bool {
 		}
 		return n.parent.Balance(children[pivot_index].key, low_node, high_node)
 	}
+	low.SetParent(n)
+	high.SetParent(n)
 	n.children = children
 	return true
 }
 
 func (n *InternalNode) Insert(k string, v uint64) bool {
-	i, _ := slices.BinarySearchFunc(n.children, &Child {k, 0, nil, nil}, func(a, b *Child) int {
+	i, _ := slices.BinarySearchFunc(n.keys, k, func(a, b *Child) int {
 		return strings.Compare(a.key, b.key)
 	})
+	if i >= len(n.children) {
+		i = len(n.children) - 1
+	}
 	pivot_key := n.children[i].key
 	if strings.Compare(k, pivot_key) <= 0 {
 		return n.children[i].left.Insert(k, v)
@@ -127,11 +238,8 @@ func (n *LeafNode) Insert(k string, v uint64) bool {
 		pivot_index := len(children) / 2
 		low_records := children[0: pivot_index]
 		high_records := children[pivot_index:]
-		low_node := newLeafNode(n.m, low_records, n.parent)
-		high_node := newLeafNode(n.m, high_records, n.parent)
-		if n.parent == nil {
-			n.parent = newRootNode(n.m)
-		}
+		low_node := newLeafNode(n.m, low_records, nil)
+		high_node := newLeafNode(n.m, high_records, nil)
 		return n.parent.Balance(children[pivot_index].key, low_node, high_node)
 	}
 	n.children = children
@@ -139,11 +247,14 @@ func (n *LeafNode) Insert(k string, v uint64) bool {
 }
 
 func (n *InternalNode) Find(k string) (uint64, error) {
-	i, _ := slices.BinarySearchFunc(n.children, &Child {k, 0, nil, nil }, func(a, b *Child) int {
+	i, found := slices.BinarySearchFunc(n.children, &Child {k, 0, nil, nil }, func(a, b *Child) int {
 		return strings.Compare(a.key, b.key)
 	})
+	if !found && i >= len(n.children) {
+		i = len(n.children) - 1
+	}
 	pivot_key := n.children[i].key
-	if strings.Compare(k, pivot_key) <= 0 {
+	if strings.Compare(k, pivot_key) < 0 {
 		return n.children[i].left.Find(k)
 	}
 	return n.children[i].right.Find(k)
@@ -153,15 +264,9 @@ func (n *LeafNode) Find(k string) (uint64, error) {
 	i, found := slices.BinarySearchFunc(n.children, &Child{k, 0, nil, nil}, func(a, b *Child) int {
 		return strings.Compare(a.key, b.key)
 	})
-	fmt.Printf("Find %v", n.children)
+	fmt.Printf("Find %v\n", n.children)
 	if !found {
 		return 0, &NotFoundError{ fmt.Sprintf("No record found for %s",k) }
 	}
 	return n.children[i].value, nil
-}
-
-func newLeafNode(m int, children []*Child, parent TreeNode) TreeNode {
-	return &LeafNode {
-		m, children, parent,
-	}
-}
+}*/
--- db/b_tree_test.go
+++ db/b_tree_test.go
@@ -2,8 +2,15 @@ package db
 
 import (
 	"testing"
+	"slices"
+	"strings"
+	"math/rand"
+	"time"
+	//"fmt"
+	//"reflect"
 )
 
+/*
 func TestInsertAndFind(t *testing.T) {
 	root := newRootNode(2)
 	root.Insert("1", 1)
@@ -16,24 +23,24 @@ func TestInsertAndFind(t *testing.T) {
 	}
 }
 
-func TestInsertAndFind(t *testing.T) {
+func TestInsertAndFindTwoLevels(t *testing.T) {
 	root := newRootNode(2)
 	root.Insert("1", 1)
 	root.Insert("2", 2)
 	root.Insert("3", 3)
 
-	actual, _ := root.Find("2")
+	actual, _ := root.Find("3")
 
 	expected_root := newRootNode(2)
 	c1 := newChild("1", 1, nil, nil)
 	c2 := newChild("2", 2, nil, nil)
 	c3 := newChild("3", 3, nil, nil)
-	left := newLeafNode(2, []*Child{c1, c2}, nil)
-	right := newLeafNode(2, []*Child{c3}, nil)
+	left := newLeafNode(2, []*Child{c1}, nil)
+	right := newLeafNode(2, []*Child{c2, c3}, nil)
 	child := newChild("2", 0, left, right)
 	i_node := newInternalNode(2, []*Child{child}, root)
-	left.parent = i_node
-	right.parent = i_node
+	left.SetParent(i_node)
+	right.SetParent(i_node)
 	expected_root.child = i_node
 
 	if actual != 3 {
@@ -44,3 +51,155 @@ func TestInsertAndFind(t *testing.T) {
 		t.Errorf("Expected %v. Actual %v", root, expected_root)
 	}
 }
+
+func TestInsertAndFindThreeLevels(t *testing.T) {
+	root := newRootNode(2)
+	root.Insert("1", 1)
+	root.Insert("2", 2)
+	root.Insert("3", 3)
+	root.Insert("4", 4)
+
+	actual, _ := root.Find("2")
+
+	expected_root := newRootNode(2)
+	c1 := newChild("1", 1, nil, nil)
+	c2 := newChild("2", 2, nil, nil)
+	c3 := newChild("3", 3, nil, nil)
+	c4 := newChild("4", 4, nil, nil)
+	l1 := newLeafNode(2, []*Child{c1}, nil)
+	l2 := newLeafNode(2, []*Child{c2}, nil)
+	l3 := newLeafNode(2, []*Child{c3, c4}, nil)
+	i1 := newChild("2", 0, l1, l2)
+	i2 := newChild("3", 0, l2, l3)
+	i_node := newInternalNode(2, []*Child{i1, i2}, root)
+	l1.SetParent(i_node)
+	l2.SetParent(i_node)
+	l3.SetParent(i_node)
+	expected_root.child = i_node
+
+	if actual != 2 {
+		t.Errorf("Expected %v. Actual %v", 2, actual)
+	}
+
+	if !reflect.DeepEqual(root, expected_root) {
+		t.Errorf("Expected %v. Actual %v", root, expected_root)
+	}
+}
+
+func TestInsertAndFindMultipleInternalNodes(t *testing.T) {
+	root := newRootNode(2)
+	root.Insert("1", 1)
+	root.Insert("2", 2)
+	root.Insert("3", 3)
+	root.Insert("4", 4)
+	root.Insert("5", 5)
+
+	actual, _ := root.Find("4")
+
+	expected_root := newRootNode(2)
+	c1 := newChild("1", 1, nil, nil)
+	c2 := newChild("2", 2, nil, nil)
+	c3 := newChild("3", 3, nil, nil)
+	c4 := newChild("4", 4, nil, nil)
+	//c5 := newChild("5", 4, nil, nil)
+	l1 := newLeafNode(2, []*Child{c1}, nil)
+	l2 := newLeafNode(2, []*Child{c2}, nil)
+	l3 := newLeafNode(2, []*Child{c3, c4}, nil)
+	i1 := newChild("2", 0, l1, l2)
+	i2 := newChild("3", 0, l2, l3)
+	i_node := newInternalNode(2, []*Child{i1, i2}, root)
+	l1.SetParent(i_node)
+	l2.SetParent(i_node)
+	l3.SetParent(i_node)
+	expected_root.child = i_node
+
+	if actual != 4 {
+		t.Errorf("Expected %v. Actual %v", 2, actual)
+	}
+
+	if !reflect.DeepEqual(root, expected_root) {
+		t.Errorf("Expected %v. Actual %v", root, expected_root)
+	}
+}*/
+
+func TestBinarySearchLesserOrGreater(t *testing.T) {
+	keys := []string{"a", "c", "e", "g", "i"}
+	actual_index := BinarySearch(keys, "b", 0, len(keys))
+	expected_index := 1
+
+	if actual_index != expected_index {
+		t.Errorf("Expected %d. Actual %d", expected_index, actual_index)
+	}
+}
+
+func TestBinarySearchEqual(t *testing.T) {
+	keys := []string{"a", "c", "e", "g", "i"}
+	actual_index := BinarySearch(keys, "c", 0, len(keys))
+	expected_index := 2
+
+	if actual_index != expected_index {
+		t.Errorf("Expected %d. Actual %d", expected_index, actual_index)
+	}
+}
+
+func TestBinarySearchEdge(t *testing.T) {
+	keys := []string{"a", "c", "e", "g", "i"}
+	actual_index := BinarySearch(keys, "j", 0, len(keys))
+	expected_index := len(keys)
+
+	if actual_index != expected_index {
+		t.Errorf("Expected %d. Actual %d", expected_index, actual_index)
+	}
+}
+
+func TestQuickSort(t *testing.T) {
+	keys := generateStringArrays(1, 5)
+	expected_sorted_keys := make([]string, len(keys))
+	copy(expected_sorted_keys, keys)
+	slices.SortFunc(expected_sorted_keys, func(a, b string) int {
+			return strings.Compare(strings.ToLower(a), strings.ToLower(b))
+	})
+	QuickSort(keys)
+
+	if !slices.Equal(expected_sorted_keys, keys) {
+		t.Errorf("Expected %v. Actual %v", expected_sorted_keys, keys)
+	}
+}
+
+func TestQuickSortTwoItems(t *testing.T) {
+	keys := generateStringArrays(1, 2)
+	expected_sorted_keys := make([]string, len(keys))
+	copy(expected_sorted_keys, keys)
+	slices.SortFunc(expected_sorted_keys, func(a, b string) int {
+			return strings.Compare(strings.ToLower(a), strings.ToLower(b))
+	})
+	QuickSort(keys)
+
+	if !slices.Equal(expected_sorted_keys, keys) {
+		t.Errorf("Expected %v. Actual %v", expected_sorted_keys, keys)
+	}
+}
+
+func generateString(m int) string {
+	seededRand := rand.New(rand.NewSource(time.Now().UnixNano()))
+	charset := "abcdefghijklmnopqrstuvwxyz"
+	b := make([]byte, m)
+	for i := range b {
+		b[i] = charset[seededRand.Intn(len(charset))]
+	}
+	// Using a strings.Builder can be more efficient than direct string
+	// concatenation
+	var sb strings.Builder
+	sb.Write(b)
+	return sb.String()
+}
+
+func generateStringArrays(m int, n int) []string {
+	vals := make([]string, n)
+	for i := 0; i < n; i++ {
+		s := generateString(m)
+		vals[i] = s
+	}
+	return vals
+}
+
